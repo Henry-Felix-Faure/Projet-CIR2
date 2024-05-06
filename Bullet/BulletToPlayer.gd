ca38941192f7233fa2106faf887cdb4d3eda920f
Destro122 <romeo.soccio@gmail.com>
2024-05-06 16:15:03 +0200

    Added shooting behaviour

---
+++ Bullet/BulletToPlayer.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@onready var visible_on_screen: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
+
+func _ready():
+	visible_on_screen.screen_exited.connect(queue_free)
+
+func _physics_process(delta):
+	const SPEED = 1000
+	const RANGE = 1000
+	var direction = Vector2.RIGHT.rotated(rotation)
+	position += direction * SPEED * delta 
+		
+
+func _on_body_entered(body):
+	queue_free()
+
