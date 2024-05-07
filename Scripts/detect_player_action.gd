2a444e57c3b33e9af2f88a61fd09ef7b87646109
Destro122 <romeo.soccio@gmail.com>
2024-05-07 16:21:56 +0200

    Added basic ennemies ranged behaviours
    Still need to do some improuvements

--- Scripts/detect_player_action.gd
+++ Scripts/detect_player_action.gd
@@ -1,9 +1,12 @@
 extends Area2D
 
 @export var ComponentFonctionToCallFrom : Node
+signal player_in
+signal player_out
 
 func _ready() -> void:
 	body_entered.connect(call_function_with_player)
-
+	
 func call_function_with_player(body):
 	ComponentFonctionToCallFrom.call_func(body)
+
