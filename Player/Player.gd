a0b3de6fa6cbf28042d79be208eb86d221ad3664
FF-Henry <henry.felix-faure@student.junia.com>
2024-05-07 11:38:38 +0200

    attack in four direction

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,11 +5,18 @@ const MAX_SPEED = 100
 const ACCELERATION = 700
 const FRICTION = 1400
 
+enum{
+	MOVING,
+	DASHING,
+	ATTACKING
+}
+
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D # importing the animation sprite
-var last_dir: Vector2 = Vector2.ZERO # Vector2 of the last direction faced for idling animations
+var last_dir: Vector2 = Vector2(0, 1) # Vector2 of the last direction faced for idling animations
+var state = MOVING
 
 func _ready():
-	animated_sprite_2d.play("idle_down") # starting animation
+	motion_mode = 1
 
 func next_animation_selector_moving(input_vector: Vector2): # function to decide which running animations we want to play
 	if input_vector.x > 0: # if the player moving towards the right
@@ -43,8 +50,23 @@ func next_animation_selector_idling(): # function to decide which idling animati
 		animated_sprite_2d.play("idle_down")
 	elif last_dir.y < 0: # if the player was moving towards top
 		animated_sprite_2d.play("idle_up")
+		
+#func _input(event):
+	#if event is InputEventMouseButton:
+		#print("Mouse Click/Unclick at: ", event.position)
+	#elif event is InputEventMouseMotion:
+		#print("Mouse Motion at: ", event.position)
 
 func _physics_process(delta): 
+	match state: # switch case to call the function associated to the action
+		MOVING:
+			move_state(delta)
+		DASHING:
+			pass
+		ATTACKING:
+			attack_state(delta)
+
+func move_state(delta):
 	var input_vector = Vector2.ZERO # resetting the input vector
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left") # setting the direction for next move by checking which key is pressed (left or right, or both (not moving))
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up") # setting the direction for next move by checking which key is pressed (top or bottom, or both (not moving))
@@ -56,5 +78,21 @@ func _physics_process(delta):
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta) # updating velocity while taking the parameters in consideration (FRICTION)
 		next_animation_selector_idling() # calling the function to select the right idling animations
-
+	
 	move_and_slide() # moving the character based on the velocity
+	
+	if Input.is_action_just_pressed("ui_attack"): # if left click is pressed
+		state = ATTACKING # changing the state to ATTACK
+
+func attack_state(delta):
+	if last_dir.x != 0: # if the player was moving towards left or right
+		animated_sprite_2d.play("atk_right_1") # playing the correct animation (same for the other if/elif)
+		await animated_sprite_2d.animation_finished # waiting for the animation to finish (same for the other if/elif)
+	elif last_dir.y < 0: # if the player was moving towards bottom
+		animated_sprite_2d.play("atk_up_1")
+		await animated_sprite_2d.animation_finished
+	elif last_dir.y > 0: # if the player was moving towards bottom
+		animated_sprite_2d.play("atk_down_1")
+		await animated_sprite_2d.animation_finished
+		
+	state = MOVING # changing the state to MOVING
