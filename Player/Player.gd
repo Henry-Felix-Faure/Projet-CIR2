a841f3376bc0f02bae043d23dd0c211352dc9648
FF-Henry <henry.felix-faure@student.junia.com>
2024-05-07 15:08:20 +0200

    attack combo in four directions (with tempo)

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
 # initial variables for moving
-const MAX_SPEED = 100
-const ACCELERATION = 700
-const FRICTION = 1400
+@export var MAX_SPEED: int = 100
+var ACCELERATION: int = 10000
+var FRICTION: int = 10000
 
 enum{
 	MOVING,
@@ -12,8 +12,18 @@ enum{
 }
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D # importing the animation sprite
+@onready var animation_player: AnimationPlayer = $AnimationPlayer # importing the animation player
+
 var last_dir: Vector2 = Vector2(0, 1) # Vector2 of the last direction faced for idling animations
 var state = MOVING
+
+var attacks_array: Array = [
+	["atk_right_1", "atk_right_2", "atk_right_3"], 
+	["atk_down_1", "atk_down_2", "atk_down_3"], 
+	["atk_up_1", "atk_up_2", "atk_up_3"]
+]
+
+var attack_counter: int = 0
 
 func _ready():
 	motion_mode = 1
@@ -67,6 +77,8 @@ func _physics_process(delta):
 			attack_state(delta)
 
 func move_state(delta):
+	MAX_SPEED = 100
+	
 	var input_vector = Vector2.ZERO # resetting the input vector
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left") # setting the direction for next move by checking which key is pressed (left or right, or both (not moving))
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up") # setting the direction for next move by checking which key is pressed (top or bottom, or both (not moving))
@@ -82,17 +94,44 @@ func move_state(delta):
 	move_and_slide() # moving the character based on the velocity
 	
 	if Input.is_action_just_pressed("ui_attack"): # if left click is pressed
+		attack_counter += 1
 		state = ATTACKING # changing the state to ATTACK
 
-func attack_state(delta):
+func next_animation_selector_attacking():
 	if last_dir.x != 0: # if the player was moving towards left or right
-		animated_sprite_2d.play("atk_right_1") # playing the correct animation (same for the other if/elif)
-		await animated_sprite_2d.animation_finished # waiting for the animation to finish (same for the other if/elif)
-	elif last_dir.y < 0: # if the player was moving towards bottom
-		animated_sprite_2d.play("atk_up_1")
-		await animated_sprite_2d.animation_finished
+		animated_sprite_2d.play(attacks_array[0][attack_counter-1]) # playing the correct animation of attack (same for the other if/elif)
 	elif last_dir.y > 0: # if the player was moving towards bottom
-		animated_sprite_2d.play("atk_down_1")
-		await animated_sprite_2d.animation_finished
+		animated_sprite_2d.play(attacks_array[1][attack_counter-1])
+	elif last_dir.y < 0: # if the player was moving towards bottom
+		animated_sprite_2d.play(attacks_array[2][attack_counter-1])
 		
+	match attack_counter: # switch case to play the right tempo for attack
+		1:
+			animation_player.play("atk_1_speed")
+		2:
+			animation_player.play("atk_2_speed")
+		3:
+			animation_player.play("atk_3_speed")
+
+func attack_state(delta): # function who is handling the different case of attack
+	next_animation_selector_attacking() # call the function to play the right animation
+	
+	if Input.is_action_just_pressed("ui_attack") and attack_counter < 3: # if left click is pressed
+		attack_counter += 1
+	
+	var input_vector = Vector2.ZERO # resetting the input vector
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left") # setting the direction for next move by checking which key is pressed (left or right, or both (not moving))
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up") # setting the direction for next move by checking which key is pressed (top or bottom, or both (not moving))
+	input_vector = input_vector.normalized() # we need to normalize because if we move diagonally we will move sqrt(2) pixel instead of 1 pixel
+	
+	if input_vector != Vector2.ZERO: # if we detect a move to execute
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta) # updating velocity while taking the parameters in consideration (MAX_SPEED, ACCELERATION)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta) # updating velocity while taking the parameters in consideration (FRICTION)
+	
+	move_and_slide() # moving the character based on the velocity
+	
+	await animated_sprite_2d.animation_finished # waiting for the animation to finish 
+	
+	attack_counter = 0
 	state = MOVING # changing the state to MOVING
