8983c071156cf4b89dde80fc4aefc889b06d8950
AntoineWart <antoine.wartel@student.junia.com>
2024-05-07 16:31:20 +0200

    ajout premier mob

--- enemy/CAC/mob_cac.gd
+++ enemy/CAC/mob_cac.gd
@@ -1,35 +1,25 @@
 extends CharacterBody2D
 
-var health: int = 100
-var speed: float = 600.0
-var attack_damage: int = 20
-var attackInterval : float = 2.0
-const expScene = preload("res://experience.tscn")
+const SPEED: float = 1000.0
+
+const expScene = preload("res://experience/experience.tscn")
+const EnnemiesStatsComponent = preload("res://Scripts/Components/ennemies_stats_component.gd")
+var ennemies_stats_component := EnnemiesStatsComponent.new()
+
 
 @onready var range: Area2D = $range
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-@onready var player = get_node("/root/World/player")
+@onready var player = get_node("/root/World/Player")
 @onready var wait_timer: Timer = $Timer
 var wait : bool = false
 var in_area : bool = false
 
+
 func _ready():
+	var attackInterval = ennemies_stats_component.ATK_SPEED / 100
 	wait_timer.wait_time = attackInterval
 	range.body_entered.connect(_on_range_body_entered)
 	range.body_exited.connect(_on_range_body_exited)
-	
-func take_damage(amount: int) -> void:
-	health -= amount
-	if health <= 0:
-		die()
-
-
-func die() -> void:
-	var exp = expScene.instantiate()
-	exp.position = position
-	get_tree().current_scene.add_child(exp)
-	queue_free()
-	
 
 
 func attack() -> void:
@@ -40,11 +30,21 @@ func attack() -> void:
 
 
 func _physics_process(delta):
-	if(!wait):
+	if !wait:
+		var direction = global_position.direction_to(player.global_position)
+		velocity = direction * SPEED * delta
+		move_and_slide()
+		var temp_pos = direction.x
+
+		if direction.x < 0:
+			animated_sprite_2d.set_flip_h(true)
+		else:
+			animated_sprite_2d.set_flip_h(false)
+		
+		
 		animated_sprite_2d.play("move")
-	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * speed * delta * 5
-	move_and_slide()
+
+
 
 
 func _on_range_body_entered(body):
