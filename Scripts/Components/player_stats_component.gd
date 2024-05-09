964c125adc60b95e1576ccef567850720763c6a2
FF-Henry <henry.felix-faure@student.junia.com>
2024-05-09 16:53:34 +0200

    Revert "Merge branch 'main' into Henry"
    This reverts commit caf5f0bb2c4eb19071936a6dd934ae7c471379fe, reversing
    changes made to 357f5dcbcbefe8f48892e2e5a7ecfa844741764e.

--- Scripts/Components/player_stats_component.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-@export var SPEED : int = 100
-@export var ATK : int = 100
-@export var ATKSPEED : int = 100
-@export var CRIT : int = 100
-@export var DASH_SPEED : int = 100
-@export var DASH_CD : int = 100
--- Scripts/detect_player_action.gd
+++ Scripts/detect_player_action.gd
@@ -1,14 +1,10 @@
 extends Area2D
 
 @export var ComponentFonctionToCallFrom : Node
-@export var AttackSpeed : Timer
-signal player_in
-signal player_out
 
 func _ready() -> void:
 	body_entered.connect(call_function_with_player)
-	
-func call_function_with_player(body):
-	if(AttackSpeed.get_time_left() == 0):
-		ComponentFonctionToCallFrom.call_func(body)
 
+func call_function_with_player(body):
+	print_debug("DEBUG")
+	ComponentFonctionToCallFrom.call_func(body)
